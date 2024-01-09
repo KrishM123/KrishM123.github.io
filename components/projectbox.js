9ab9e744a2904450793565566c0a16e1912f31f7
KrishM123 <kahnscatterkrish@gmail.com>
2024-01-09 16:50:21 -0500

    added sci fair

--- components/projectbox.js
+++ components/projectbox.js
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-export default function RootLayout({ image, link, title }) {
+export default function RootLayout({ image, link, title, description }) {
     return (
         <Link href={link}>
             <Image
@@ -13,8 +13,11 @@ export default function RootLayout({ image, link, title }) {
                 alt={link}
                 className={"rounded-xl shadow-lg hover:shadow-2xl"}
                 />
-            <div className={"flex flex-grow justify-center font-sans font-bold color-darkest text-center"}>
+            <div className={"flex flex-grow justify-center font-sans text-sm font-bold color-darkest text-center"}>
                 {title}
+            </div>
+            <div className={"flex flex-grow justify-center font-sans text-sm italic color-darkest text-center"}>
+                {description}
             </div>
         </Link>
     )
